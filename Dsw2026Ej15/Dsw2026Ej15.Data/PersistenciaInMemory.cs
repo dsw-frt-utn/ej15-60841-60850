4e270709c63cb1bf64768bedd4fd3a163030daa5
Bautista Ferrari Osado <BautistaFerrariOsado@alu.frt.utn.edu.ar>
2026-06-13 18:57:03 -0300

    Arreglar Persistencia

--- Dsw2026Ej15/Dsw2026Ej15.Data/PersistenciaInMemory.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenciaInMemory.cs
@@ -1,9 +1,11 @@
 ﻿using Dsw2026Ej15.Domain.Entities;
 using Dsw2026Ej15.Domain.Interfaces;
+using Dsw2026Ej15.Data.Dta;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+
 
 namespace Dsw2026Ej15.Data;
 
@@ -13,30 +15,31 @@ public class PersistenciaInMemory : IPersistence
     private readonly List<Speciality> Specialities = [];
 
     public PersistenciaInMemory(){}
+
     public static async Task<PersistenciaInMemory> InitializeDataAsync()
     {
         var persistencia = new PersistenciaInMemory();
-        await persistencia.InitializeSpecialities();
-        await persistencia.InitializeDoctors();
+        persistencia.InitializeDoctors();
         return persistencia;
     }
     public async Task InitializeDoctors()
     {
-        var doctorsData = await LoadData<Doctor>("doctors");
-        if (doctorsData != null)
-        {
-            foreach (var data in doctorsData)
-            {
-                var speciality = Specialities.Find(s => s.Id == data.Id);
-                Doctor s = new Doctor(data.Id, data.Name, data.LicenseNumber, data.IsActive,data.Speciality);
-                Doctors.Add(s);
-            }
-        }
+        await InitializeSpecialities();
+        var s1 = Specialities.Find(s => s.Name("Pediatría"));
+        var s2 = Specialities.Find(s => s.Name("Neurología"));
+        var s3 = Specialities.Find(s => s.Name("Cardiología"));
 
+        var d1 = new Doctor("31b4fc6c - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Joaquin Valdecantos", "MN-45213", true, s1);
+        var d2 = new Doctor("31b4fc67 - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Juan Tenreyro", "MM-45213", false, s2);
+        var d3 = new Doctor("31b4fc62 - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Tadeo Torres", "MR-45213", true, s3);
+        
+        Doctors.Add(d1);
+        Doctors.Add(d2);
+        Doctors.Add(d3);
     }
     public async Task InitializeSpecialities()
     {
-        var specialitiesData = await LoadData<Speciality>("specialities");
+        var specialitiesData = await LoadSpecialities<SpecialityDta>("specialities");
         if(specialitiesData != null)
         {
             foreach (var data in specialitiesData)
@@ -46,7 +49,7 @@ public class PersistenciaInMemory : IPersistence
             }
         }
     }
-    private async Task<List<T>?> LoadData<T>(string file)
+    private async Task<List<T>?> LoadSpecialities<T>(string file)
     {
         string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Sources", $"{file}.json");
         var jsonContent = await File.ReadAllTextAsync(jsonPath);
