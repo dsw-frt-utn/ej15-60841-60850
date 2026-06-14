48db5b892b5464f2a6c3992b8dd03b197b320098
Juan Tenreyro <tenreyrojuan1@gmail.com>
2026-06-14 16:12:06 -0300

    Fix bugs

--- Dsw2026Ej15/Dsw2026Ej15.Data/PersistenciaInMemory.cs
+++ Dsw2026Ej15/Dsw2026Ej15.Data/PersistenciaInMemory.cs
@@ -25,13 +25,13 @@ public class PersistenciaInMemory : IPersistence
     public async Task InitializeDoctors()
     {
         await InitializeSpecialities();
-        var s1 = Specialities.Find(s => s.Name("Pediatría"));
-        var s2 = Specialities.Find(s => s.Name("Neurología"));
-        var s3 = Specialities.Find(s => s.Name("Cardiología"));
+        var s1 = Specialities.Find(s => s.Name == "Pediatría");
+        var s2 = Specialities.Find(s => s.Name == "Neurología");
+        var s3 = Specialities.Find(s => s.Name == "Cardiología");
 
-        var d1 = new Doctor("31b4fc6c - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Joaquin Valdecantos", "MN-45213", true, s1);
-        var d2 = new Doctor("31b4fc67 - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Juan Tenreyro", "MM-45213", false, s2);
-        var d3 = new Doctor("31b4fc62 - 8594 - 4b51 - 93a0 - 7b2432a51f89", "Tadeo Torres", "MR-45213", true, s3);
+        var d1 = new Doctor(Guid.Parse("31b4fc6c - 8594 - 4b51 - 93a0 - 7b2432a51f89"), "Joaquin Valdecantos", "MN-45213", true, s1);
+        var d2 = new Doctor(Guid.Parse("31b4fc67 - 8594 - 4b51 - 93a0 - 7b2432a51f89"), "Juan Tenreyro", "MM-45213", false, s2);
+        var d3 = new Doctor(Guid.Parse("31b4fc62 - 8594 - 4b51 - 93a0 - 7b2432a51f89"), "Tadeo Torres", "MR-45213", true, s3);
         
         Doctors.Add(d1);
         Doctors.Add(d2);
